594517772d602d64613bb8987c6946c5f991c363
Adam A <adam@hack.bg>
2024-12-03 12:25:12 +0100

    test: a route

--- tests/test.js
+++ tests/test.js
@@ -4,6 +4,8 @@ const console = new Console("Undexer Test Suite")
 import process from 'node:process'
 import { PostgresMock } from "pgmock"
 import { freePort } from "@hackbg/port"
+
+const t0 = performance.now()
 const port = await freePort()
 const mock = await PostgresMock.create()
 const conn = await mock.listen(port)
@@ -15,5 +17,22 @@ console.log('Importing database module...')
 const { default: db, initDb } = await import('../src/db.js')
 console.log('Setting up mock database...')
 await initDb()
-console.log('Mock database ready.')
+console.log('Syncing mock DB schema...')
+await db.sync()
+console.log('Mock database ready in', performance.now() - t0)
+
+const { dbRoutes } = await import('../src/dbRoutes.js')
+const testRoute = (route, query = {}) =>
+  new Promise(resolve=>route({ query }, {
+    code: null,
+    data: null,
+    status (code) { this.code = code; return this },
+    send (data) { this.data = data; resolve(this) },
+  }))
+const testDbRoute = (path, query) => testRoute(dbRoutes[path], query)
+
+const t1 = performance.now()
+console.log(await testDbRoute('/'))
+console.log('Tests done in', performance.now() - t1)
+
 mock.destroy()

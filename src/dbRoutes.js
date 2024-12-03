3838f4d27de599df8687b0be83cb3ddd2852c10f
Skunk Works <adam@hack.bg>
2024-12-03 08:01:45 +0000

    chore(api): use imperative conditional in includeDelegated

--- src/dbRoutes.js
+++ src/dbRoutes.js
@@ -183,10 +183,10 @@ dbRoutes['/validator'] = async function dbValidatorByHash (req, res) {
 dbRoutes['/validator/votes/:address'] = async function dbValidatorVotes(req, res) {
   const { limit, offset } = pagination(req);
   const includeDelegated = req.query.delegated && req.query.delegated === 'true'
-  const where = {
-    ...(includeDelegated ?
-      {} : { isValidator: true }), validator: req.params.address
-  };
+  const where = { validator: req.params.address }
+  if (!includeDelegated) {
+    where.isValidator = true
+  }
   const order = [['proposal', 'DESC']]
   const attrs = Query.defaultAttributes();
   const { count, rows } = await DB.Vote.findAndCountAll({

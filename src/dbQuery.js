51d80b668a06bde5a225c24c0e50f3dc40f67443
Adam A <adam@hack.bg>
2024-12-20 20:31:22 +0100

    feat(api): combined bonds/unbonds table

--- src/dbQuery.js
+++ src/dbQuery.js
@@ -351,28 +351,41 @@ export const deactivateValidatorList = async ({
   ORDER BY "blockHeight" DESC LIMIT :limit OFFSET :offset
 `, { type: SELECT, replacements: { address: JSON.stringify(address), limit, offset, } })
 
+const bondOrUnbondFilter = ({ source, validator }) => sql.fragment`
+  WHERE ${or(matchContentType("tx_bond.wasm"), matchContentType("tx_unbond.wasm"))}
+    AND ${matchSourceOrValidator({ source, validator })}`
+const bondFilter = ({ source, validator }) => sql.fragment`
+  WHERE ${matchContentType("tx_bond.wasm")}
+    AND ${matchSourceOrValidator({ source, validator })}`
+const unbondFilter = ({ source, validator }) => sql.fragment`
+  WHERE ${matchContentType("tx_unbond.wasm")}
+    AND ${matchSourceOrValidator({ source, validator })}`
+const bondUnbondPagination = ({ limit, offset }) => paginateByContent(
+  "content", sql.fragment`'data'->>'amount'`, sql.fragment`bigint`, DESC, limit, offset)
+
+export const bondOrUnboundCount = ({ source = "", validator = "" }) =>
+  slonikCount(sql.unsafe`SELECT COUNT(*)
+    ${fromTxsByContent} ${bondOrUnbondFilter({ source, validator })}`)
+export const bondOrUnboundList = ({ source, validator, limit = 100, offset = 0 }) =>
+  slonikQuery(sql.unsafe`SELECT *
+    ${fromTxsByContent} ${bondOrUnbondFilter({ source, validator })}
+    ${bondUnbondPagination({ limit, offset })}`)
+
 export const bondCount = ({ source = "", validator = "" }) =>
-  slonikCount(sql.unsafe`
-    SELECT COUNT(*) ${fromTxsByContent}
-    WHERE ${matchContentType("tx_bond.wasm")}
-    AND ${matchSourceOrValidator({ source, validator })}`)
+  slonikCount(sql.unsafe`SELECT COUNT(*)
+    ${fromTxsByContent} ${bondFilter({ source, validator })}`)
 export const bondList = ({ source, validator, limit = 100, offset = 0 }) =>
-  slonikQuery(sql.unsafe`
-    SELECT * ${fromTxsByContent}
-    WHERE ${matchContentType("tx_bond.wasm")}
-    AND ${matchSourceOrValidator({ source, validator })}
-    ${paginateByContent("content", sql.fragment`'data'->>'amount'`, sql.fragment`bigint`, DESC, limit, offset)}`)
+  slonikQuery(sql.unsafe`SELECT *
+    ${fromTxsByContent} ${bondFilter({ source, validator })}
+    ${bondUnbondPagination({ limit, offset })}`)
+
 export const unbondCount = ({ source = "", validator = "" }) =>
-  slonikCount(sql.unsafe`
-    SELECT COUNT(*) ${fromTxsByContent}
-    WHERE ${matchContentType("tx_unbond.wasm")}
-    AND ${matchSourceOrValidator({ source, validator })}`)
+  slonikCount(sql.unsafe`SELECT COUNT(*)
+    ${fromTxsByContent} ${unbondFilter({ source, validator })}`)
 export const unbondList = ({ source, validator, limit = 100, offset = 0 }) =>
-  slonikQuery(sql.unsafe`
-    SELECT * ${fromTxsByContent}
-    WHERE ${matchContentType("tx_unbond.wasm")}
-    AND ${matchSourceOrValidator({ source, validator })}
-    ${paginateByContent("content", sql.fragment`'data'->>'amount'`, sql.fragment`bigint`, DESC, limit, offset)}`)
+  slonikQuery(sql.unsafe`SELECT *
+    ${fromTxsByContent} ${unbondFilter({ source, validator })}
+    ${bondUnbondPagination({ limit, offset })}`)
 
 export const txWithAddressCount = async ({ address = "" }) => await count(`
   SELECT COUNT(*) FROM "transactions" WHERE (
